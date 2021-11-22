534271bd5170b96383e247b8e79249fbd64afcc0
Kieran <kimmurkj@plu.edu>
2021-11-22 10:32:47 -0800

    feat: generate room impulse response
    Configure sensor array
    Add array to room
    Compute RIR
    Plot RIR

--- room-acoustics/room-acoustics.py
+++ room-acoustics/room-acoustics.py
@@ -1,6 +1,8 @@
 import pyroomacoustics as pra
 from os.path import dirname, join as pjoin
 from scipy.io import wavfile
+import numpy as np
+import matplotlib.pyplot as plt
 
 CURRENT_DIR = 'room-acoustics'
 AUDIO_DIR = 'audio'
@@ -23,3 +25,19 @@ samplerate, audio = wavfile.read(wav_fname)
 
 # PLACE SOURCE IN ROOM
 room.add_source([2.5, 3.73, 1.76], signal=audio, delay=1.3)
+
+# CONFIGURE SENSOR ARRAY
+mic_locs = np.c_[
+    [6.3, 4.87, 1.2],  # mic 1
+    [6.3, 4.93, 1.2],  # mic 2
+]
+
+# PLACE ARRAY IN ROOM
+room.add_microphone_array(mic_locs)
+
+# COMPUTE RIR
+room.compute_rir()
+
+# PLOT RIR BETWEEN MIC 1 AND SOURCE 0
+plt.plot(room.rir[1][0])
+plt.show()
