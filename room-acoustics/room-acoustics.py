39bc826531af6f9f8373fb952711881d9e4abea1
Kieran <kimmurkj@plu.edu>
2021-12-05 23:24:41 -0800

    feat: functionalize source placement and wav file generation
    Move sound source placement and wav file generation into a generate_wav function, so that we can reuse the code.
    Implemented a plot function to marginally streamline using plt.
    Move rest of script into a main method.

--- room-acoustics/room-acoustics.py
+++ room-acoustics/room-acoustics.py
@@ -1,3 +1,4 @@
+import pyroomacoustics
 import pyroomacoustics as pra
 from os.path import dirname, join as pjoin
 from scipy.io import wavfile
@@ -31,67 +32,57 @@ def center_point_on_origin(origin: list[int], point: list[int]) -> list[int]:
     return [a+b for a, b in zip(origin, point)]
 
 
-# LOAD AUDIO SOURCE
-data_dir = pjoin(dirname(__file__), 'dataverse_files', 'Recordings').replace(CURRENT_DIR, AUDIO_DIR)
-wav_fname = pjoin(data_dir, FILE_NAME)
-samplerate, audio = wavfile.read(wav_fname)
-
-plt.plot(audio) # Audio before simulation
-plt.show()
-
-# ROOM SETUP
-rt60 = 1.0  # seconds, reverb time
-room_dim = [ROOM_WIDTH, ROOM_HEIGHT, ROOM_WIDTH]  # meters, room dimensions
-
-e_absorption, max_order = pra.inverse_sabine(rt60, room_dim)
-
-print(max_order)
-room = pra.ShoeBox(
-    room_dim, fs=samplerate, materials=pra.Material(e_absorption), max_order=10 # hardcoded a lower reflection order
-)
-
-# PLACE SOURCE IN ROOM
-room.add_source([2.5, 3.73, 1.76], signal=audio, delay=1.3)
-
-# CONFIGURE SENSOR ARRAY
-square_array = [center_point_on_origin(ORIGIN, sensor) for sensor in SQUARE_DELTA]
-
-mic_locs = np.c_[
-    square_array[0],  # mic 1
-    square_array[1],  # mic 2
-    square_array[2],  # mic 3
-    square_array[3],  # mic 4
-]
-
-# PLACE ARRAY IN ROOM
-room.add_microphone_array(mic_locs)
-
-# COMPUTE RIR
-room.compute_rir()
-
-# PLOT RIR BETWEEN MIC 1 AND SOURCE 0
-plt.plot(room.rir[1][0])
-plt.show()
-
-# CONVOLVE RIR AND SOURCES
-room.simulate()
-
-# PLOT SIGNALS
-for channel in room.mic_array.signals:
-    plt.plot(channel)
+# So I don't need to dedicated two whole lines to plotting some data
+def plot(data: list[np.ndarray]):
+    plt.plot([row for row in data])
     plt.show()
 
-plt.plot(room.mic_array.signals[0, :])
-plt.plot(room.mic_array.signals[1, :])
-plt.plot(room.mic_array.signals[2, :])
-plt.plot(room.mic_array.signals[3, :])
-plt.show()
 
-# WRITE TO WAV FILE
-room.mic_array.to_wav(
-    f"output/fishsound_{time.time()}.wav",
-    norm=True,
-    bitdepth=np.float32
-)
+# Accepts a room, a source file, an output path, a location, and an optional delay and saves a reverberant
+# transformation of the source signal to a wav file in the specified location.
+def generate_wav(room: pra.Room, filename: str, output_path: str, source_location: list[int], source_delay: float = 1.3):
+    sample_rate, data = wavfile.read(filename)
+    plot(data) # Plot source signal before transformation
+    room.add_source(source_location, signal=data, delay=source_delay)
+    room.compute_rir()
+    plot(room.rir[1][0]) # Plot RIR between Mic 1 and Src 0
+    room.simulate()
+    # TODO: Plot source signal after it gets convolved with the RIR
+    room.mic_array.to_wav(
+        output_path,
+        norm=True,
+        bitdepth=np.float32,
+    )
 
 
+def get_path(filename: str) -> str:
+    data_dir = pjoin(dirname(__file__), 'dataverse_files', 'Recordings').replace(CURRENT_DIR, AUDIO_DIR)
+    return pjoin(data_dir, filename)
+
+
+def main():
+    samplerate, audio = wavfile.read(get_path(FILE_NAME))
+    # TODO: input file is currently read in here, because we need the samplerate, and also in the generate_wav function
+
+    # Configure room
+    rt60 = 1.0  # seconds, reverb time
+    room_dim = [ROOM_WIDTH, ROOM_HEIGHT, ROOM_WIDTH]  # meters, room dimensions
+    e_absorption, max_order = pra.inverse_sabine(rt60, room_dim)
+    room = pra.ShoeBox(
+        room_dim, fs=samplerate, materials=pra.Material(e_absorption), max_order=10 # hardcoded a lower reflection order
+    )
+
+    # Configure sensor array
+    square_array = [center_point_on_origin(ORIGIN, sensor) for sensor in SQUARE_DELTA]
+    sensor_locations = np.transpose(square_array)
+
+    # Add sensor array to room
+    room.add_microphone_array(sensor_locations)
+
+    #Generate WAV file
+    location = [2.5, 3.73, 1.76]
+    generate_wav(room, get_path(FILE_NAME), f"output/generated{time.time()}.wav", location)
+
+
+if __name__ == "__main__":
+    main()
